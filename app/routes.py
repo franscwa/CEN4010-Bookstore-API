242987e5d7553e246491851bda1c4a9ff1abd6d3
David Giles <Dgiles98@gmail.com>
2023-07-11 17:25:47 -0400

    skeleton

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,19 @@ app = create_app()
 @app.route('/')
 def hello_world():
     return jsonify(message="Hello, World!")
+
+@app.route('/byGenre')
+def booksByGenre():
+    return "", 204
+
+@app.route('/byTopSellers')
+def booksByTopSellers():
+    return "", 204
+
+@app.route('/byRating')
+def booksByRating():
+    return "", 204
+
+@app.route('/byPublisher', methods=['PUT'])
+def booksByPublisher():
+    return "", 204
