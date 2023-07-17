88992774065b22d8dfcd1b49fa86a3ee6ccdc16e
David Giles <Dgiles98@gmail.com>
2023-07-17 18:10:12 -0400

    models added and paths updated

--- app/routes.py
+++ app/routes.py
@@ -1,52 +1,62 @@
-from flask import request, jsonify, render_template
-from flask_mysqldb import MySQL
-from . import create_app, db_service
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from services.db_service import db
+from models.models import Book
 
-app = create_app()
+app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'username'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'bookstore'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Caestus#98@server/db'
 
-mysql = MySQL(app)
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
     return jsonify(message="Hello, World!")
 
-@app.route('/byGenre')
-def booksByGenre():
-    sql = mysql.connection.cursor()
-    sql.execute("SELECT Name, AuthorID, Price FROM books ORDER BY Genre DESC")
-    books = sql.fetchall()
-    sql.close()
-    return render_template('genres.html', books=books)
+@app.route('/books/<genre>', methods=['GET'])
+def get_books_by_genre(genre):
+    books = Book.query.filter_by(genre=genre).all()
+    book_list = []
+    for book in books:
+        book_data = {
+            'id': book.id,
+            'title': book.title,
+            'genre': book.genre
+        }
+        book_list.append(book_data)
+    return jsonify({'books': book_list})
 
-@app.route('/byTopSellers')
-def booksByTopSellers():
-    sql = mysql.connection.cursor()
-    sql.execute("SELECT Name, AuthorID, Price FROM books ORDER BY CopiesSold DESC")
-    books = sql.fetchall()
-    sql.close()
-    return render_template('genres.html', books=books)
+@app.route('/books/most_sold', methods=['GET'])
+def get_most_sold_books():
+    most_sold_books = Book.query.order_by(Book.sales.desc()).limit(10).all()
+    book_list = []
+    for book in most_sold_books:
+        book_data = {
+            'id': book.id,
+            'title': book.title,
+            'sales': book.sales
+        }
+        book_list.append(book_data)
+    return jsonify({'most_sold_books': book_list})
 
-@app.route('/byRating')
-def booksByRating():
-    sql = mysql.connection.cursor()
-    sql.execute("SELECT Name, AuthorID, Price FROM books ORDER BY rating DESC")
-    books = sql.fetchall()
-    sql.close()
-    return render_template('genres.html', books=books)
+@app.route('/books/higher_rating/<float:threshold>', methods=['GET'])
+def get_books_with_higher_rating(threshold):
+    books = Book.query.filter(Book.rating > threshold).all()
+    book_list = []
+    for book in books:
+        book_data = {
+            'id': book.id,
+            'title': book.title,
+            'rating': book.rating
+        }
+        book_list.append(book_data)
+    return jsonify({'books_with_higher_rating': book_list})
 
-@app.route('/discountPublisher', methods=['POST'])
-def discountPublisher():
-    publisher = request.form['publisher']
-    discount_percentage = request.form['new_price']
-
-    cur = mysql.connection.cursor()
-    cur.execute("UPDATE books SET price = price - (price * %s / 100) WHERE publisher = %s",
-                    (discount_percentage, publisher))
-    mysql.connection.commit()
-    cur.close()
-    return "", 204
+@app.route('/books/discount_publisher/<publisher>/<float:discount>', methods=['PUT'])
+def discount_books_by_publisher(publisher, discount):
+    books = Book.query.filter_by(publisher=publisher).all()
+    for book in books:
+        book.price -= (book.price * discount)
+        db.session.add(book)
+    db.session.commit()
+    return jsonify({'message': 'Discount applied to books by publisher.'})
