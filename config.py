cd488790dac8c2f4b801ca46175493bcb4edb0dc
Brandon Wayne George <33503621+bdongeorge1@users.noreply.github.com>
2023-07-17 22:55:52 -0400

    Feature 1 Database Creation

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or         'mysql://user:password@localhost/dbname'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://root:@localhost/wishlist_database'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
