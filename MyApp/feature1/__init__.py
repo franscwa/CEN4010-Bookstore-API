442a6671def310c1f4fc8a15aedc2d65b82a02f9
Brandon Wayne George <33503621+bdongeorge1@users.noreply.github.com>
2023-07-25 14:45:31 -0400

    My app feature 1

--- MyApp/feature1/__init__.py
+++ MyApp/feature1/__init__.py
@@ -1,9 +1,92 @@
-from BrandonApp import db
+from flask import Flask, request, jsonify
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+# Database configuration
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'wishlist_database'
+
+mysql = MySQL(app)
+
+def init_db():
+    cur = mysql.connection.cursor()
+    cur.execute('''  
+        CREATE TABLE IF NOT EXISTS wishlist (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            user_id INT NOT NULL,
+            name VARCHAR(255) NOT NULL
+        ) 
+        ''')
+    cur.execute(''' 
+        CREATE TABLE IF NOT EXISTS wishlist_item (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            wishlist_id INT NOT NULL,
+            book_id INT NOT NULL,
+            FOREIGN KEY (wishlist_id) REFERENCES wishlist(id)
+        ) 
+        ''')
+    cur.execute(''' 
+        CREATE TABLE IF NOT EXISTS book (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            author VARCHAR(255) NOT NULL
+        ) 
+        ''')
+    mysql.connection.commit()
+
+# Creates tables if not exist
+with app.app_context():
+    init_db()
+
+@app.route('/wishlist', methods=['POST'])
+def create_wishlist():
+    cur = mysql.connection.cursor()
+    user_id = request.json['user_id']
+    name = request.json['name']
+    cur.execute("INSERT INTO wishlist(user_id, name) VALUES(%s, %s)", (user_id, name))
+    mysql.connection.commit()
+    cur.close()
+    return jsonify({'message': 'Wishlist created successfully!'}), 201
 
 
-class Wishlist(db.Model):
-    __tablename__ = 'wishlist'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
-    name = db.Column(db.String(255))
+@app.route('/wishlist/<int:wishlist_id>/book/<int:book_id>', methods=['POST'])
+def add_book_to_wishlist(wishlist_id, book_id):
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO wishlist_item(wishlist_id, book_id) VALUES(%s, %s)", (wishlist_id, book_id))
+    mysql.connection.commit()
+    cur.close()
+    return jsonify({'message': 'Book added to wishlist successfully!'}), 201
 
+@app.route('/wishlist/<int:wishlist_id>/books', methods=['GET'])
+def get_books_in_wishlist(wishlist_id):
+    cur = mysql.connection.cursor()
+    cur.execute('''
+        SELECT book.id, book.title, book.author
+        FROM book
+        INNER JOIN wishlist_item ON book.id = wishlist_item.book_id
+        WHERE wishlist_item.wishlist_id = %s
+    ''', (wishlist_id,))
+    books = [dict(id=row[0], title=row[1], author=row[2]) for row in cur.fetchall()]
+    cur.close()
+    return jsonify(books), 200
+
+@app.route('/wishlist/<int:wishlist_id>/book/<int:book_id>', methods=['DELETE'])
+def remove_book_from_wishlist(wishlist_id, book_id):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM wishlist_item WHERE wishlist_id=%s AND book_id=%s", (wishlist_id, book_id))
+    mysql.connection.commit()
+    cur.close()
+    if cur.rowcount == 0:
+        return jsonify({'message': 'No such book in the wishlist'}), 404
+    else:
+        return jsonify({'message': 'Book removed from wishlist successfully!'}), 200
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ profile-management/views.py
@@ -0,0 +1,75 @@
+from flask import Flask, request, jsonify
+from flask_pymysql import PyMySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'user'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'mydb'
+mysql = PyMySQL(app)
+
+# Endpoint to create a new user
+@app.route('/user', methods=['POST'])
+def create_user():
+    # Extract information from the request
+    user_data = request.get_json()
+    username = user_data['username']
+    password = user_data['password']
+    # Optional fields
+    name = user_data.get('name')
+    email = user_data.get('email')
+    home_address = user_data.get('home_address')
+
+    # Insert the new user into the database
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO users (username, password, name, email, home_address) VALUES (%s, %s, %s, %s, %s)", 
+                (username, password, name, email, home_address))
+    mysql.connection.commit()
+    cur.close()
+
+    return '', 204
+
+# Endpoint to get a user by username
+@app.route('/user/<username>', methods=['GET'])
+def get_user(username):
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+    if result > 0:
+        user_data = cur.fetchone()
+        return jsonify(user_data)
+    else:
+        return 'User not found', 404
+
+# Endpoint to update a user
+@app.route('/user/<username>', methods=['PUT', 'PATCH'])
+def update_user(username):
+    # Get the updated fields from the request
+    updated_data = request.get_json()
+
+    # Update the user in the database
+    cur = mysql.connection.cursor()
+    for key, value in updated_data.items():
+        if key != 'email':
+            cur.execute("UPDATE users SET {}=%s WHERE username=%s".format(key), (value, username))
+    mysql.connection.commit()
+    cur.close()
+
+    return '', 204
+
+# Endpoint to create a credit card for a user
+@app.route('/user/<username>/credit_card', methods=['POST'])
+def create_credit_card(username):
+    # Extract credit card details from the request
+    card_data = request.get_json()
+
+    # Insert the new credit card into the database
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO credit_cards (username, card_number, expiry_date, cvv) VALUES (%s, %s, %s, %s)", 
+                (username, card_data['card_number'], card_data['expiry_date'], card_data['cvv']))
+    mysql.connection.commit()
+    cur.close()
+
+    return '', 204
+
+if __name__ == '__main__':
+    app.run(debug=True)
